69832c72e6caa734535245b35043c0d893c2d1ff
xzw <1272117264@qq.com>
2018-07-05 05:31:51 +0800

    代码promise化

--- main.js
+++ main.js
@@ -7,31 +7,25 @@ window.jQuery=function(nodeOrSelector) {
     return nodes
 }
 window.jQuery.ajax=function({url,method,body,successFn,failFn,headers}){
-/*    let url
-    //判断传入的参数是两个还是一个
-    if(arguments.length===1){
-        url=options.url
-    }else if(arguments.length===2){
-        url=arguments[0]
-        options=arguments[1]
-    }*/
-    //ES6语法
-    let request=new XMLHttpRequest()
-    request.open(method,url)
-    for(let key in headers){
-        let value=headers[key]
-        request.setRequestHeader(key,value)
-    }
-    request.send(body)
-    request.onreadystatechange=()=>{
-        if(request.readyState===4){
-            if(request.status>=200&&request.status<300){
-                successFn.call(undefined,request.responseText)
-            }else if(request.status>=400){
-                failFn.call(undefined,request)
+    //如何去写一个promise
+    return new Promise(function(resolve,reject){
+        let request=new XMLHttpRequest()
+        request.open(method,url)
+        for(let key in headers){
+            let value=headers[key]
+            request.setRequestHeader(key,value)
+        }
+        request.onreadystatechange=()=>{
+            if(request.readyState===4){
+                if(request.status>=200&&request.status<300){
+                    resolve.call(undefined,request.responseText)
+                }else if(request.status>=400){
+                    reject.call(undefined,request)
+                }
             }
         }
-    }
+        request.send(body)
+    })
 
 }
 
